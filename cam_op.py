b53e13e9f851018ade6571d78321134bb047148c
Delepoulle Samuel <delepoulle@lisic.univ-littoral.fr>
2019-12-12 15:12:45 +0100

    add look-at position

--- cam_op.py
+++ cam_op.py
@@ -1,12 +1,22 @@
 import bpy
 from bpy_extras.io_utils import ExportHelper
-
+from mathutils import Vector
 
 def write_cameras(context, filepath) :
 
     fw = open(filepath, 'w').write
-    obj_camera = bpy.context.scene.camera
-    fw(str(obj_camera.location[0])+" "+str(obj_camera.location[1])+" "+str(obj_camera.location[2])+" ")
+
+    # write position
+    obj_camera = bpy.context.scene.camera   
+    fw(str(obj_camera.location[0])+" "+str(obj_camera.location[1])+" "+str(obj_camera.location[2])+"\n")
+
+    # write look-at
+    cam_direction = obj_camera.matrix_world.to_quaternion() @ Vector((0.0, 0.0, -1.0))
+    lookat = obj_camera.location+cam_direction
+    fw(str(lookat[0])+" "+str(lookat[1])+" "+str(lookat[2])+" ")
+
+
+
 
 
 class CameraExporter(bpy.types.Operator, ExportHelper):
